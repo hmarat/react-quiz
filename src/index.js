4784c43297458327064880878d7514f194109dcd
hmarat <marat.hakobjanyan9@gmail.com>
2019-10-14 10:16:50 +0400

    Started using redux in react-quiz

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom"
+import {createStore, applyMiddleware, compose} from "redux"
+import {Provider} from "react-redux"
+import thunk from "redux-thunk"
+import rootReducer from "./store/reducers/rootReducer"
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
 
 const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 )
 ReactDOM.render(app, document.getElementById('root'));
 
